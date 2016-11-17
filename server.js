cdc33e93596734006a4d3a31c417b043cb250814
Juan <juancruzg69@gmail.com>
2016-11-17 14:46:07 -0300

    Agrego un get sobre la raíz para siempre redireccionar a la web cuando no se le pega al API

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ var port = process.env.PORT || 9000;
 
 app.use('/api/users', require('./api/users'));
 app.use('/api/tasks', require('./api/tasks'));
+
 app.use('/', express.static("./web"));
+
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + "/web/index.html");
+});
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
