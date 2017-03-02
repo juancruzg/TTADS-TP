7e3ba58df3962c77f5e4b086480d3992a980b77b
Juan Cruz Grasso <juancruzg69@gmail.com>
2017-03-01 23:10:18 -0300

    testing

--- test/user.test.js
+++ test/user.test.js
@@ -1,11 +1,15 @@
 var chai = require("chai");
+var chaiHttp = require('chai-http');
+var http = require('http');
 
-var controller = require("./../api/uesrs/user.controller.js");
+chai.use(chaiHttp);
 
 var expect = chai.expect;
 
 describe("Users", function() {
-  it("getUsers() returns 0 when no items.", function() {
-    expect(controller.getUsers()).to.equal(0);
+  chai.request('http://localhost:9000').get('/api/users').end(function (err, res) {
+    it("getUsers() returns 0 when no items.", function(done) {
+
+    });
   });
 });
